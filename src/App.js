5fcf94463a3428a40250cd357248c5ca5d3ed80d
Prashant singh <71235386+prashant5025@users.noreply.github.com>
2022-10-12 09:49:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -45,7 +45,20 @@ const App = () => {
             <Route path='/orders' element="Orders"/>
             <Route path='/employees' element="Employees"/>
             <Route path='/customers' element="Customers"/>
-
+            {/* Apps  */}
+            <Route path='/kanban' element="Kanban" />
+            <Route path='/editor' element="Editor" />
+            <Route path='/calendar' element="Calendar"/>
+            <Route path='/color-picker' element="ColorPicker" />
+            {/* Charts  */}
+            <Route path="/line" element="Line"/>
+            <Route path="/area" element="Area"/>
+            <Route path="/bar" element="Bar" />
+            <Route path="/pie" element="Pie" />
+            <Route path="/financial" element="Financial"/>
+            <Route path="/color-mapping" element="ColorMapping"/>
+            <Route path="/pyramid" element="Pyramid"/>
+            <Route path="/stacked" element="Stacked"/>
           </Routes>
         </div>
       </div>
