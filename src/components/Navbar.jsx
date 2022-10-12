7346d25401b9b121c9f29f45471541e6f31579cb
Prashant Singh <psingh5025615@gmail.com>
2022-10-13 03:40:09 +0530

    Navbar started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useEffect,} from 'react';
+import {AiOutlineMenu} from 'react-icons/ai'
+import {FiShoppingCart} from 'react-icons/fi'
+import {BsChatLeft} from 'react-icons/bs'
+import {RiNotification3Line} from 'react-icons/ri'
+import {MdKeyboardArrowDown} from 'react-icons/md'
+
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import avatar from '../data/avatar.jpg';
+import {Cart, Charts, Notification, UserProfile} from '.';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Navbar = () => {
+  const {activeMenu, setActiveMenu} = useStateContext();
   return (
     <div>
       
