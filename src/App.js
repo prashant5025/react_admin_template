358b7fd7dbd8a2142e5c3d79bb8edada5bc29b5a
Prashant Singh <psingh5025615@gmail.com>
2022-10-12 06:00:19 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React,{useEffect} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {FiSettings} from 'react-icons/fi';
+import {TooltipComponent} from '@syncfusion/ej2-react-popups';
+
+const App = () => {
+
+  const activeMenu = false;
+
+  return (
+    <div>
+      <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'>
+        <div className='fixed right-4 bottom-4' style={{zIndex: "1000"}}>
+          <TooltipComponent content="Settings" position="Top">
+            <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+            style={{background: 'skyblue', borderRadius: '50%'}}>
+            
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+            Sidebar
+          </div>
+        ) : (
+          <div className='w-0 dark:bg-secondary-dark-bg'>
+            Sidebar
+          </div>
+        )}
+        <div className={
+          `dark:bg-main-bg bg-main-bg main-h-screen w-full ${activeMenu ? 'ml-72' : 'flex-2'}`
+        }>
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            Navbar
+          </div>
+        </div>
+        <div>
+          <Routes>
+            {/* Dashboard  */}
+            <Route path='/' element="Ecommerce"/>
+            <Route path='/ecommerce' element="Ecommerce"/>
+            {/* Pages  */}
+            <Route path='/orders' element="Orders"/>
+            <Route path='/employees' element="Employees"/>
+            <Route path='/customers' element="Customers"/>
+
+          </Routes>
+        </div>
+      </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
