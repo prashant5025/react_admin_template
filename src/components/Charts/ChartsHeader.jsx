84ff064d125a5b8b2c059946d198932b341852d8
Prashant Singh <psingh5025615@gmail.com>
2022-10-12 16:04:42 +0530

    structuring complete

--- src/components/Charts/ChartsHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ChartHeader = () => {
-  return (
-    <div>ChartHeader</div>
-  )
-}
-
-export default ChartHeader
---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChartsHeader = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChartsHeader
